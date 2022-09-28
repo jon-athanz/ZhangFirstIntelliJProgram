208608d147b492f623d862a3626c6081b3786ce1
jon-athanz <jonathanz28@nycstudents.net>
2022-09-28 08:46:23 -0400

    added variables and a print statement!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        int number1;
+        int number2;
+
+        number1 = 4;
+        number2 = 6;
+
+        System.out.println(number1 + number2);
+        System.out.println("Hello World!");
+    }
 }
